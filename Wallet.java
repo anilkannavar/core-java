a9cecc40bea966974588703ad87d060f56903f83
anilkannavar <149233835+anilkannavar@users.noreply.github.com>
2023-11-01 22:55:28 +0530

    super_task

---
+++ Wallet.java
@@ -0,0 +1,19 @@
+class Wallet
+{
+  static String brand="goorej";
+  static String cost="30rs";
+  static long length=30;
+  static short size=560;
+  static String type="leather";
+  
+  static void displayWallet()
+  {
+	  System.out.println("showing displayWallet in socket");
+	  System.out.println("brand:"+brand);
+	  System.out.println("cost:"+cost);
+	  System.out.println("length:"+length);
+	  System.out.println("size:"+size);
+	  System.out.println("type:"+type);
+	  System.out.println("ending displayWallet in socket");
+  }  
+}
