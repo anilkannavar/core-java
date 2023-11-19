4603d9178b8334e237888a01886b98a250d9b73a
anilkannavar <anil29.xworkz@gmail.com>
2023-11-19 23:53:28 +0530

    type casting

---
+++ Generator.java
@@ -0,0 +1,11 @@
+class Generator extends Machine
+{
+	Generator()
+	{
+		System.out.println("creating no-arg const in Generator");
+	}
+	void display()
+	{
+		System.out.println("Running display in generator");
+	}
+}
