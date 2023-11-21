dd67a92c4e3a97d9a0ddb7209a2f2791e9aaee7d
anilkannavar <anil29.xworkz@gmail.com>
2023-11-21 10:29:57 +0530

    inheritance

---
+++ Person3.java
@@ -0,0 +1,15 @@
+class Person3
+{
+	Person3()
+	{
+		System.out.println("creating no-arg const in Person");
+	}
+	void eat()
+	{
+		System.out.println("Running eat in person");
+	}
+	void sleep()
+	{
+		System.out.println("Running sleep in person");
+	}
+}
