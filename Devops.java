0ef95b613fadfe2aec29e019fdd678d4245b29ac
anilkannavar <anil29.xworkz@gmail.com>
2023-11-21 10:34:05 +0530

    inheritance

---
+++ Devops.java
@@ -0,0 +1,11 @@
+class Devops extends Person3
+{
+	Devops()
+	{
+	    System.out.println("creating no-arg const in Devops");
+    }
+	void runBuild()
+	{
+		System.out.println("Running runBuild in Devops");
+    }
+}
