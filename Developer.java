96746a3ebfe606993e5da1561f4eeac47344d69d
anilkannavar <anil29.xworkz@gmail.com>
2023-11-21 10:30:33 +0530

    inheritance

---
+++ Developer.java
@@ -0,0 +1,12 @@
+class Developer extends Person3
+{
+	Developer()
+	{
+		System.out.println("creating no-arg const in Developer");
+    }
+	void code()
+	{
+	    System.out.println("Running code in Developer");
+
+	}
+}
