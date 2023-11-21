d3456abf6d4e965268d23a548ebdd439a6e6f8af
anilkannavar <anil29.xworkz@gmail.com>
2023-11-21 20:40:24 +0530

    chaining in inheritance

---
+++ TintedGlass.java
@@ -0,0 +1,7 @@
+class TintedGlass extends ToughenedGlass
+{
+	TintedGlass(int length,double thickness,String usage,String brand,GlassType type,double cost,int warrenty,double weight,String ownerName)
+	{
+		super(length,thickness,usage,brand,type,cost,warrenty,weight,ownerName);
+	}
+}
