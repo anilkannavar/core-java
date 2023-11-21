da625e22d1832d64f41894c931a6757924b4520b
anilkannavar <anil29.xworkz@gmail.com>
2023-11-21 10:32:24 +0530

    inheritance

---
+++ ProductOwner.java
@@ -0,0 +1,11 @@
+class ProductOwner extends Person3
+{
+	ProductOwner()
+	{
+		System.out.println("creating no-arg const in ProductOwner");
+    }
+	void validate()
+	{
+		System.out.println("Running validate in validate");
+    }
+}
