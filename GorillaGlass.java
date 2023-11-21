a7584325e789f3c2f2cb2335f393279a2ffaaf8a
anilkannavar <anil29.xworkz@gmail.com>
2023-11-21 20:41:26 +0530

    chaining in inheritance

---
+++ GorillaGlass.java
@@ -0,0 +1,7 @@
+class GorillaGlass extends LaminatedGlass
+{
+	GorillaGlass(int length,double thickness,String usage,String brand,GlassType type,double cost,int warrenty)
+	{
+		super(length,thickness,usage,brand,type,cost,warrenty);
+	}
+}
