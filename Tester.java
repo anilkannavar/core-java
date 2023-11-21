56afdd4eee1910597d1c6fb28306bb01bf251259
anilkannavar <anil29.xworkz@gmail.com>
2023-11-21 10:31:07 +0530

    inheritance

---
+++ Tester.java
@@ -0,0 +1,11 @@
+class Tester extends Developer
+{
+	Tester()
+	{
+		System.out.println("creating no-arg const in Tester");
+	}
+	void qualityCheck()
+	{
+		System.out.println("Running qualityCheck in Tester");
+    }
+}
