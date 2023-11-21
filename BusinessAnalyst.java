2da0eadb5a85c2368f32c0985f0155719456f14e
anilkannavar <anil29.xworkz@gmail.com>
2023-11-21 10:31:48 +0530

    inheritance

---
+++ BusinessAnalyst.java
@@ -0,0 +1,11 @@
+class BusinessAnalyst extends Person3
+{
+	BusinessAnalyst()
+	{
+		System.out.println("creating no-arg const in BusinessAnalyst");
+    }
+	void checkRequirement()
+	{
+		System.out.println("Running checkRequirement in BusinessAnalyst");
+    }
+}
